af88ea90c580e051a4f324f0cc2a557a5e5fc2a5
wardtd2 <wardtd@gmail.com>
2024-04-29 18:45:43 -0400

    Fixed the first test and wrote a few more, then implemented the normalizeURL function to pass the tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,12 @@
 function normalizeURL(urlString) {
-    return;
+    const url = new URL(urlString);
+    let path = url.pathname;
+    
+    if (path.charAt(path.length - 1) == '/') {
+        path = path.slice(0, -1);
+    }
+
+    return `${url.hostname}${path}`;
 }
 
 export { normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -3,5 +3,19 @@ import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
 
 test('Converts https://blog.boot.dev/path/ to blog.boot.dev/path', () => {
-    expect(normalizeURL('https://blog.boot.dev/path/').toBe('blog.boot.dev/path'))
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
+
+
+test('Converts https://blog.boot.dev/path to blog.boot.dev/path', () => {
+    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+
+test('Converts http://blog.boot.dev/path/ to blog.boot.dev/path', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
+
+test('Converts http://blog.boot.dev/path to blog.boot.dev/path', () => {
+    expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
 });
