df03f793177670da0870ca72f41b276d5d423ad8
wardtd2 <wardtd@gmail.com>
2024-04-30 11:18:59 -0400

    Started writing tests for pulling an array of URLs from a string of HTML. Figuring out how to incorporate JSDOM

--- crawl.js
+++ crawl.js
@@ -2,11 +2,19 @@ function normalizeURL(urlString) {
     const url = new URL(urlString);
     let path = url.pathname;
     
-    if (path.charAt(path.length - 1) == '/') {
+    if (path.charAt(path.length - 1) === '/') {
         path = path.slice(0, -1);
     }
 
     return `${url.hostname}${path}`;
 }
 
-export { normalizeURL };
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    const urls = dom.window.document.querySelectorAll('a');
+    console.log(urls);
+    return urls;
+}
+
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,6 @@
 
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
 
 test('Converts https://blog.boot.dev/path/ to blog.boot.dev/path', () => {
     expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
@@ -16,6 +16,34 @@ test('Converts http://blog.boot.dev/path/ to blog.boot.dev/path', () => {
     expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
 });
 
+
 test('Converts http://blog.boot.dev/path to blog.boot.dev/path', () => {
     expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+
+test('Retrives a single URL from the provided html', () => {
+    const html = '<html><body><a href="https://blog.boot.dev/path"><span>Go to Boot.dev</span></a></body></html>';
+    const baseURL = 'blog.boot.dev'
+    const expected = ['https://blog.boot.dev/path'];
+    expect(getURLsFromHTML(html, baseURL)).toBe(expected);
+});
+
+
+test('Converts relative URL to absolute', () => {
+    const html = '<html><body><a href="/games/path/"><span>Go to Boot.dev</span></a></body></html>';
+    const baseURL = 'blog.boot.dev'
+    const expected = ['blog.boot.dev/games/path/'];
+    expect(getURLsFromHTML(html, baseURL)).toBe(expected);
+});
+
+
+test('Retrieves multiple URLs from HTML', () => {
+    const html = `<html><body><a href="/games/path/"><span>Go to Boot.dev</span></a><a href="/games/patha/"><span>Go to Boot.dev</span></a>
+    <a href="http://blog.boot.dev/path"><span>Go to Boot.dev</span></a>
+    <a href="https://blog.boot.dev/path/banana"><span>Go to Boot.dev</span></a>
+    </body></html>`;
+    const baseURL = 'blog.boot.dev'
+    const expected = ['blog.boot.dev/games/path/', 'blog.boot.dev/games/patha/', 'http://blog.boot.dev/path', 'https://blog.boot.dev/path/banana'];
+    expect(getURLsFromHTML(html, baseURL)).toBe(expected);
 });
